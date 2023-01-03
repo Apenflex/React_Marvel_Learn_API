b48701267f4a28201eb1a86f80c5eeb25a255ce3
Sviatoslav <blackdbf@gmail.com>
2023-01-03 13:42:09 +0200

    added functional for character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,3 @@
-
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=e25c3e9ac3a4fb8c128c53a3fa2e0a06';
@@ -13,12 +12,23 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 }
 
